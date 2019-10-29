0febadb4ec836fb28c98d09bdd46f55d66c89a6a
rushilk <rushilk@umich.edu>
2019-10-28 22:41:00 -0400

    Fixed exponent test

--- test_rpn.py
+++ test_rpn.py
@@ -4,7 +4,7 @@ import rpn
 class TestBasics(unittest.TestCase):
     def test_exp(self):
         result = rpn.calculate("3 3 ^")
-        self.assert(27, result)
+        self.assertEqual(27, result)
     def test_mult(self):
         result = rpn.calculate("4 3 *")
         self.assertEqual(12, result)
