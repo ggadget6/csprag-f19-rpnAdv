76609b034a7f38c03abd879cc5d09098dbbf9e9d
rushilk <rushilk@umich.edu>
2019-10-28 22:11:11 -0400

    Implement subtract and add
    This change implements both add and subtract in the rpn calculator

--- rpn.py
+++ rpn.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
 
 def calculate(arg):
-    pass
+    stack = list()
+    for token in arg.split():
+        if token == '+':
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 + arg2
+            stack.append(result)
+        elif token == '-':
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            result = arg1 - arg2
+            stack.append(result)
+        else:
+            stack.append(int(token))
+
+        #print(stack)
+    if len(stack) != 1:
+        raise TypeError('malformed input')
+    return stack.pop()
 
 def main():
     while True:
