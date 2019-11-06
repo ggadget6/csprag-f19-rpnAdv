922f6ccfd16bd36addee5e6f628d298370ae0544
rushilk <rushilk@umich.edu>
2019-11-06 13:18:37 -0500

    Added colored operators and updated travis file accordingly

--- rpn.py
+++ rpn.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import readline
+from colorama import Fore, Back, Style
+
 def calculate(arg):
     stack = list()
     for token in arg.split():
@@ -7,28 +10,35 @@ def calculate(arg):
             arg2 = stack.pop()
             arg1 = stack.pop()
             result = arg1 + arg2
+            print(Fore.RED + token, end = ' ')
             stack.append(result)
         elif token == '-':
             arg2 = stack.pop()
             arg1 = stack.pop()
             result = arg1 - arg2
+            print(Fore.RED + token, end = ' ')
             stack.append(result)
         elif token == '*':
             arg2 = stack.pop()
             arg1 = stack.pop()
             result = arg1 * arg2
+            print(Fore.RED + token, end = ' ')
             stack.append(result)
         elif token == '^':
             arg2 = stack.pop()
             arg1 = stack.pop()
             result = arg1 ** arg2
+            print(Fore.RED + token, end = ' ')
             stack.append(result)
         elif token == '/':
             arg2 = stack.pop()
             arg1 = stack.pop()
             result = arg1 // arg2
+            print(Fore.RED + token, end = ' ') 
             stack.append(result)
         else:
+            print(Style.RESET_ALL, end = '')
+            print(int(token), end = ' ')
             stack.append(int(token))
 
         #print(stack)
@@ -38,7 +48,12 @@ def calculate(arg):
 
 def main():
     while True:
-        calculate(input("rpn calc>"))
+       temp =  calculate(input("rpn calc>"))
+       print(Style.RESET_ALL, end = '')
+       print('=', end = ' ')
+       print(Fore.GREEN, end = '')
+       print(temp, end = '')
+       print(Style.RESET_ALL)
 
 
 if __name__ == '__main__': #Note: that's "underscore underscore n a m e..."
